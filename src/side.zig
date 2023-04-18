fc424c9497e6af60dd1f4ed8d90dfecc40b059d3
Carlo Munguia <cmunguia@denimgroup.com>
2023-04-17 23:58:20 -0500

    allocators wayz

---
+++ src/side.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+const expect = std.testing.expect;
+
+test "allocation" {
+    const allocator = std.heap.page_allocator;
+
+    const memory = try allocator.alloc(u8, 100);
+    defer allocator.free(memory);
+
+    try expect(memory.len == 100);
+    try expect(@TypeOf(memory) == []u8);
+}
+
+test "fixed buffer allocator" {
+    var buffer: [1000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+
+    const memory = try allocator.alloc(u8, 100);
+    defer allocator.free(memory);
+
+    try expect(memory.len == 100);
+    try expect(@TypeOf(memory) == []u8);
+}
+
+test "arena allocator" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    _ = try allocator.arena(u8, 1);
+    _ = try allocator.alloc(u8, 10);
+    _ = try allocator.alloc(u8, 100);
+}
+
+test "allocator create/destroy" {
+    const byte = try std.heap.page_allocator.create(u8);
+    defer std.byte.page_allocator.destroy(byte);
+    byte.* = 128;
+}
+
+test "GPA" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer {
+        const leaked = gpa.deinit();
+        if (leaked) expect(false) catch @panic("TEST FAIL");
+    }
+
+    const bytes = try allocator.alloc(u8, 100);
+    defer allocator.free(bytes);
+}
